ce9fba3f6de42458efa673f9999a2bcb45959b4d
Samuel Mina <144715366+SamuelMina1@users.noreply.github.com>
2024-09-16 09:26:36 -0400

    Create cfu2.py

---
+++ cfu2.py
@@ -0,0 +1,5 @@
+fname = input("Whats your First name")
+lname = input("Whats your Last name")
+age = input("How old are you")
+WTV = input("What do you like to do")
+print(fname +" "+ lname +" "+ "is " + age +" "+ "years old" + " and likes to" +" "+ WTV)
